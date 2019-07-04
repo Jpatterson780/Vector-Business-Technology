76a270729d4d5f0d29a10da0511c5eb77ad14918
Josh <jpatterson780@gmail.com>
2019-07-04 01:52:51 -0500

    blog

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -20,19 +20,19 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'blog' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'vectorblog' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '@Jpatt780' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define( 'AUTH_KEY',         '[9O:;SKS~p8oEWKj<P_ZHg:Zx3[4^s`t/w.:8 R aT46s{f{RB.*#4qg%=rn-:i&' );
+define( 'SECURE_AUTH_KEY',  ' 39W%c>J%6R+$k*wL2)vCwGc0z=us*Er<Rw@ro&7+!7c_6rKpby&6N7wr/mN<[Cr' );
+define( 'LOGGED_IN_KEY',    '`90)W1b:bMqzMQ{$lZ++=?]VH;u!PlD98m/PMmM4wkR+%EN=*umFfkFZCj,joZhB' );
+define( 'NONCE_KEY',        '`b@f$3SlQ,VIkI*orMKRjS*qNhQ{ElDaWz_Z1uue6h4JcbpT5H/#jU%QwvyM_0im' );
+define( 'AUTH_SALT',        'k0KC(/r w~l~si|OXXWL+:)w7vPo?E+dA[5&=x[$cMD}]!jU5Szs<AA:lJX,tKMk' );
+define( 'SECURE_AUTH_SALT', 'h$NB1-z/8w[#tWu`>COtg-((0IU>laN]U|0NNe:]AERvG0wum,UhmI;S*yA^F@u&' );
+define( 'LOGGED_IN_SALT',   'iDjV_),j.>V95<1JAP#3`HH|sGX7L;d-su<PYvi/*;#<{ra6V/_:0X  zjj-hnEG' );
+define( 'NONCE_SALT',       'bcU$v6]Y3/Myo[M>{(@6i%sU;]nM,>NkT`VCTiQX!R6pY[:P d`I{M$R.7f/4f.g' );
 
 /**#@-*/
 
